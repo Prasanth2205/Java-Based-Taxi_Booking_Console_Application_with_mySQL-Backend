e92bb79b80263b929fceaf55fd5ec004eaafd9e1
Prasanth R <136549925+Prasanth2205@users.noreply.github.com>
2023-09-28 09:07:13 +0530

    Add files via upload

---
+++ ProjectTaxi/ProjectTaxi.sql
@@ -0,0 +1,6 @@
+create database project;
+use project;
+create table taxi(phonenumber bigint primary key , Cname varchar(30), age int(3), passcode varchar(30) not null); 
+create table taxisavings(phonenumber bigint primary key ,savings decimal(6,2));
+
+
